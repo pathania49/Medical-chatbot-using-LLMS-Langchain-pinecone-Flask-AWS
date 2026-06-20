ab87b7cdfaff36e87a124ace26209c5916782163
Nipun <nipun@Nipuns-MacBook-Air.local>
2026-06-20 18:34:58 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,12 @@
+mkdir -p src
+mkdir -p research
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch app.py
+touch setup.py
+touch .env
+touch research/trials.py
+touch requirements.txt
+touch ReadME.md
+echo "succesfully created"
