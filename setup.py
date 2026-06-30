299208423af06dc063c0c79ba344b4c3db627470
Nipun <nipunpathania9@gmail.com>
2026-06-30 16:26:05 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import find_packages, setup
+setup(name="medical_chatbot",
+      version="0.1.0",
+      author="NIPUN",
+      author_email="nipunpathania9@gmail.com",
+      packages=find_packages(),
+      install_requires=[])
